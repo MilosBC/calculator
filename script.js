2261d81cb473e0bd092088a13dc6b1185aa19aa4
Milos Stefanov <144391186+MilosBC@users.noreply.github.com>
2023-12-26 01:56:07 +0100

    Write logic to populate display with data
    The buttons can be clicked to populate the display

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
 'use strict'
 
-function add(a,b) {
-    return a + b;
-}
-
-function subtract(a,b) {
-    return a - b;
-}
-
-function multiply(a,b) {
-    return a * b;
-}
-
-function divide(a,b) {
-    return a / b;
-}
-
-
-
-
-
 let numberOne;
 let operator;
 let numberTwo;
 
-function operate(a,op,b) {
+const calculator = {
+
+add: function() {
+    return a - b;
+},
+
+subtract: function(a,b) {
+    return a - b;
+},
+
+multiply: function(a,b) {
+    return a * b;
+},
+
+divide: function(a,b) {
+    return a / b;
+},
+
+operate: function(a,op,b) {
     let result;
     switch (op) {
         case '+':
-          result = add(a,b);
+          result = this.add(a,b);
           return result;
          
          case '-':
-         result = subtract(a,b);
+         result = this.subtract(a,b);
          return result;
             
          case '*':
-         result = multiply(a,b);
+         result = this.multiply(a,b);
          return result;
 
          case '/':
-         result = divide(a,b);
+         result = this.divide(a,b);
          return result; 
 
          default:
@@ -48,6 +46,102 @@ function operate(a,op,b) {
 
     }
 }
+};
 
-console.log(operate(30, '-', 3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(calculator.operate(30, '*', 3));
+
+const display = document.querySelector('.display');
+
+// Calculator numbers
+
+const numbers = Array.from(document.querySelectorAll('.number'));
+
+
+console.log(numbers);
+
+numbers.forEach((number, index) => {
+number.addEventListener('click', function() {
+    let numberIndex = numbers.indexOf(this);
+
+    if (numberIndex === 0) {
+        display.value += 7;
+    } else if (numberIndex === 1) {
+        display.value += 8;
+    } else if (numberIndex === 2) {
+        display.value += 9;
+    } else if (numberIndex === 3) {
+        display.value += 4;
+    } else if (numberIndex === 4) {
+        display.value += 5;
+    } else if (numberIndex === 5) {
+        display.value += 6;
+    } else if (numberIndex === 6) {
+        display.value += 1;
+    } else if (numberIndex === 7) {
+        display.value += 2;
+    } else if (numberIndex === 8) {
+        display.value += 3;
+    } else if (numberIndex === 9) {
+        display.value += 0;
+    }
+
+})
+
+});
+// Calculator operators
+const operators = Array.from(document.querySelectorAll('.operation'));
+
+console.log(operators);
+
+operators.forEach((operator, index) => {
+    operator.addEventListener('click', function() {
+        let operatorIndex = operators.indexOf(this);
+
+        if (operatorIndex === 0) {
+            display.value += '/';
+        } else if (operatorIndex === 1) {
+            display.value += '*';
+        } else if (operatorIndex === 2) {
+            display.value += '-';
+        } else if (operatorIndex === 3) {
+            display.value += '+';
+        }
+    })
+
+})
+
+// Calculator functions
+
+const calcFunctions = Array.from(document.querySelectorAll('.function'));
+console.log(calcFunctions);
+
+calcFunctions.forEach((calcFunction, index) => {
+    calcFunction.addEventListener('click', function() {
+        let functionIndex = calcFunctions.indexOf(this);
+
+        if (functionIndex === 0) {
+            display.value = '';
+        } 
+    })
+
+})
+
+
+
+
+
+
 
